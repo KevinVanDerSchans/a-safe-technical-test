87f0172c61a36a8c829d3de65c5dddbe2ee98291
Kevin Schans <kevinvdsd@hotmail.com>
2024-09-25 23:10:52 +0200

    Add Footer to RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import '@styles/globals.css'
 
 import Header from '@presentation/components/Header/Header'
+import Footer from '@presentation/components/Footer/Footer'
 
 export const metadata: Metadata = {
   title: 'A-SAFE Technical Test',
@@ -24,6 +25,7 @@ export default function RootLayout({
         >
           <Header />
           <main>{children}</main>
+          <Footer />
         </div>
       </body>
     </html>
