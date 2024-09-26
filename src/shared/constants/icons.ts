b3724182a5b9cb67b4b4829ab575ec654a7dfca4
Kevin Schans <kevinvdsd@hotmail.com>
2024-09-26 21:34:25 +0200

    Add Email and Password icons

--- src/shared/constants/icons.ts
+++ src/shared/constants/icons.ts
@@ -1,12 +1,14 @@
 import { FaUserAlt as PortfolioIcon, FaGithub as GitHubIcon } from 'react-icons/fa'
-import { AiFillLinkedin as LinkedInIcon } from 'react-icons/ai'
-import { MdOutlineArticle as HomeIcon } from 'react-icons/md'
+import { AiFillLinkedin as LinkedInIcon, AiOutlineMail as EmailIcon } from 'react-icons/ai'
+import { MdOutlineArticle as HomeIcon, MdOutlinePassword as PasswordIcon } from 'react-icons/md'
 
 const ICONS = {
   HomeIcon,
   PortfolioIcon,
   GitHubIcon,
   LinkedInIcon,
+  EmailIcon,
+  PasswordIcon,
 }
 
 export default ICONS
