29f8535ef9e3cd1cf458b4e8a799c8d11126909e
Kevin Schans <kevinvdsd@hotmail.com>
2024-09-27 20:05:01 +0200

    Create AuthProvider and added to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import type { AppProps } from 'next/app'
+import AuthProvider from '@context/AuthProvider'
 import MainLayout from '@layouts/MainLayout'
 import '@styles/globals.css'
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <MainLayout>
-      <Component {...pageProps} />
-    </MainLayout>
+    <AuthProvider>
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </AuthProvider>
   )
 }
 
