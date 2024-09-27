46b587d9410b7f2a5121e42ed895c6cfbde0aa43
Kevin Schans <kevinvdsd@hotmail.com>
2024-09-27 19:49:48 +0200

    Add login icon with login link

--- src/shared/constants/mainNavigationLinks.ts
+++ src/shared/constants/mainNavigationLinks.ts
@@ -3,10 +3,17 @@ import { InternalLink } from '@sharedTypes/InternalLink'
 
 export const MAIN_NAVIGATION_LINKS = (): InternalLink[] => [
   {
-    url: '/login',
+    url: '/',
     label: 'Home',
     ariaLabel: 'Go to Home page',
     icon: ICONS.HomeIcon,
     testId: 'link-go-to-home',
+  },
+  {
+    url: '/api/auth/signin',
+    label: 'Login',
+    ariaLabel: 'Go to Login page',
+    icon: ICONS.LoginIcon,
+    testId: 'link-to-login',
   },
 ]
