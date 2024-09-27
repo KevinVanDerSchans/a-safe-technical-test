8d796e54aace1376f143b4b0ae9196d7eabedfe8
Kevin Schans <kevinvdsd@hotmail.com>
2024-09-27 20:16:37 +0200

    Create dashboard page

---
+++ pages/dashboard/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Dashboard() {
+  return (
+    <>
+      <Head>
+        <title>A-SAFE Technical Test - Dashboard</title>
+        <meta
+          name='description'
+          content='Built with React, NextJS, TypeScript and Redux'
+        />
+      </Head>
+      <main className='flex items-center justify-center h-screen'>Dashboard Page</main>
+    </>
+  )
+}
