25db644907d2686a343460d6ced4663d144c1593
Kevin Schans <kevinvdsd@hotmail.com>
2024-09-27 19:46:32 +0200

    Add Login and Logged icons

--- src/shared/constants/icons.ts
+++ src/shared/constants/icons.ts
@@ -1,9 +1,12 @@
 import { FaUserAlt as PortfolioIcon, FaGithub as GitHubIcon } from 'react-icons/fa'
 import { AiFillLinkedin as LinkedInIcon, AiOutlineMail as EmailIcon } from 'react-icons/ai'
 import { MdOutlineArticle as HomeIcon, MdOutlinePassword as PasswordIcon } from 'react-icons/md'
+import { RiAdminLine as LoginIcon, RiUserHeartLine as LoggedIcon } from 'react-icons/ri'
 
 const ICONS = {
   HomeIcon,
+  LoginIcon,
+  LoggedIcon,
   PortfolioIcon,
   GitHubIcon,
   LinkedInIcon,
