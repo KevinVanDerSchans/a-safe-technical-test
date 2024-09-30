0a9b70709d3d06cefc2bb8ab6eeb09501b582e13
Kevin Schans <kevinvdsd@hotmail.com>
2024-09-30 12:33:50 +0200

    Add ThemeProvider to App component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,17 @@
 import type { AppProps } from 'next/app'
 import AuthProvider from '@context/AuthProvider'
+import { ThemeProvider } from '@context/ThemeProvider'
 import MainLayout from '@layouts/MainLayout'
-import '@styles/styles.css'
+import '@styles/globals.css'
 
 function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <AuthProvider session={session}>
-      <MainLayout>
-        <Component {...pageProps} />
-      </MainLayout>
+      <ThemeProvider>
+        <MainLayout>
+          <Component {...pageProps} />
+        </MainLayout>
+      </ThemeProvider>
     </AuthProvider>
   )
 }
