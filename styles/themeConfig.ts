bdf311cb1263bc6059454a826c9e47f8450febc6
Kevin Schans <kevinvdsd@hotmail.com>
2024-09-30 17:39:11 +0200

    Create main color palette for themes

---
+++ styles/themeConfig.ts
@@ -0,0 +1,28 @@
+const themeConfig = {
+  light: {
+    background: '#ffffff', // Color de fondo oscuro
+    foreground: '#0284c7', // Color de texto claro
+    primary: '#000000', // Elementos principales
+    secondary: '#ffffff', // Elementos secundarios
+    accent: '#1d4ed8', // Para resaltar
+    muted: '#9ca3af', // Textos secundarios
+  },
+  dark: {
+    background: '#1f2937',
+    foreground: '#d1d5db',
+    primary: '#9ca3af',
+    secondary: '#374151',
+    accent: '#4f46e5',
+    muted: '#6b7280',
+  },
+  solarized: {
+    background: '#f4ecd8',
+    foreground: '#000000',
+    primary: '#d49a6a',
+    secondary: '#b5835a',
+    accent: '#e07a5f',
+    muted: '#cfc1b0',
+  },
+}
+
+export default themeConfig
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,76 +1,18 @@
 import type { Config } from 'tailwindcss'
-import { PluginAPI } from 'tailwindcss/types/config'
+
+import themeConfig from './styles/themeConfig'
+import { tailwindPlugins } from './styles/tailwindPlugins'
 
 const config: Config = {
   mode: 'jit',
   content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}', './src/**/*.{js,ts,jsx,tsx,mdx}'],
+  darkMode: ['class', '[data-theme="dark"]'],
   theme: {
     extend: {
-      colors: {
-        primary: '#000',
-        secondary: '#fff',
-        tertiary: '#0284c7',
-
-        light: '#fff',
-        dark: '#000',
-
-        greyBackground: '#c5c5c54d',
-        errorMessage: '#dc2626',
-
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
-      },
+      colors: themeConfig,
     },
   },
-  plugins: [
-    function ({ addUtilities }: PluginAPI) {
-      addUtilities({
-        '.theme-light': {
-          '--background': '#ffffff',
-          '--foreground': '#000000',
-        },
-        '.theme-dark': {
-          '--background': '#000000',
-          '--foreground': '#ffffff',
-        },
-        '.theme-solarized': {
-          '--background': '#3df6e3',
-          '--foreground': '#657b83',
-        },
-        '.primary-hover-color-transition': {
-          'transition-property': 'color',
-          'transition-duration': '500ms',
-          color: '#fff',
-          '&:hover': {
-            color: '#0284c7',
-          },
-        },
-
-        '.primary-hover-button-transition': {
-          'transition-property': 'background-color, color',
-          'transition-duration': '500ms',
-          'background-color': '#000',
-          color: '#fff',
-          'font-weight': 'semibold',
-          '&:hover': {
-            'background-color': '#0284c7',
-            color: '#000',
-            'font-weight': 'semibold',
-          },
-        },
-
-        '.primary-hover-scale-up': {
-          'transition-property': 'transform',
-          'transition-duration': '500ms',
-          'transition-timing-function': 'ease-in-out',
-          transform: 'scale(1)',
-          '&:hover': {
-            transform: 'scale(1.25)',
-          },
-        },
-      })
-    },
-  ],
+  plugins: [tailwindPlugins],
 }
 
 export default config
