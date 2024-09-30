6b01f68c719d1f191b0a5cd0759313c8d5de5e4c
Kevin Schans <kevinvdsd@hotmail.com>
2024-09-30 21:56:08 +0200

    Check ForgotCredentials theme

--- src/presentation/features/auth/LoginForm/ForgotCredentials.tsx
+++ src/presentation/features/auth/LoginForm/ForgotCredentials.tsx
@@ -3,13 +3,13 @@ import Link from 'next/link'
 const ForgotCredentials = () => {
   return (
     <div className='flex flex-col py-4'>
-      <span className='text-black'>Have you forgotten your credentials?</span>
+      <span>Have you forgotten your credentials?</span>
 
       <Link
         href={'mailto:kevinvdsd@hotmail.com'}
         role='link'
         aria-label='Reach out to management for assistance'
-        className='underline text-tertiary hover:text-primary'
+        className='underline text-tertiary hover:text-[var(--accent)]'
       >
         Please reach out to management for assistance
       </Link>
