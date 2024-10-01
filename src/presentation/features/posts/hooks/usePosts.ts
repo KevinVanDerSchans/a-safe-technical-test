571f4a6ffd011254058d593c1610327c292cea9e
Kevin Schans <kevinvdsd@hotmail.com>
2024-10-01 13:39:30 +0200

    Create usePosts hook

---
+++ src/presentation/features/posts/hooks/usePosts.ts
@@ -0,0 +1,27 @@
+import { useCallback, useMemo } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { NEXT_PUBLIC_API_URL } from '@config/env'
+import { PostsRepository } from '@repositories/posts/PostsRepository'
+import { AppDispatch, RootState } from '@redux/store/store'
+import { getPostsAsync } from '@redux/slices/posts/postsThunks'
+
+export function usePosts() {
+  const repo = useMemo(() => new PostsRepository(NEXT_PUBLIC_API_URL), [])
+
+  const { posts, status } = useSelector((state: RootState) => state.posts)
+  const dispatch = useDispatch<AppDispatch>()
+
+  const loadPosts = useCallback(
+    async (filter?: string) => {
+      await dispatch(getPostsAsync({ repo, filter }))
+    },
+    [repo, dispatch]
+  )
+
+  return {
+    loadPosts,
+    posts,
+    status,
+  }
+}
