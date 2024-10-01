6e5507963b024b116e2c36ed1b2000b0045a2eac
Kevin Schans <kevinvdsd@hotmail.com>
2024-10-01 13:39:10 +0200

    Add postsReducer to reducers file

--- src/infrastructure/redux/store/reducers.ts
+++ src/infrastructure/redux/store/reducers.ts
@@ -1,5 +1,7 @@
+import postsReducer from '@redux/slices/posts/postsSlice'
 import usersReducer from '@redux/slices/users/usersSlice'
 
 export const rootReducer = {
+  posts: postsReducer,
   users: usersReducer,
 }
