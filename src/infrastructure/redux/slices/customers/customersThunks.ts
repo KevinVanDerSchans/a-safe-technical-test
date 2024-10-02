45098ea0ae977ebf4ad958df504d3ec4a1909ec1
Kevin Schans <kevinvdsd@hotmail.com>
2024-10-02 21:59:37 +0200

    Adjust customersThunks for security

--- src/infrastructure/redux/slices/customers/customersThunks.ts
+++ src/infrastructure/redux/slices/customers/customersThunks.ts
@@ -1,16 +1,13 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { createAsyncThunk } from '@reduxjs/toolkit'
 
-import { Customer } from '@entities/Customer'
-import { CustomersRepository } from '@repositories/customers/CustomersRepository'
-
-export const getCustomersAsync = createAsyncThunk<
-  Customer[],
-  { repo: CustomersRepository; page: number; resultsPerPage: number }
->(
+export const getCustomersAsync = createAsyncThunk<any[], { repoUrl: string; page: number; resultsPerPage: number }>(
   'customers/load',
 
-  async ({ repo, page, resultsPerPage }) => {
-    const customers = await repo.getAll(page, resultsPerPage)
-    return customers
+  async ({ repoUrl, page, resultsPerPage }) => {
+    const response = await fetch(`${repoUrl}?page=${page}&resultsPerPage=${resultsPerPage}`)
+    const data = await response.json()
+
+    return data.results
   }
 )
