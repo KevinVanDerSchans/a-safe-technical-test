07648246ff9e6d50912b09291ba72ef35b4b97b5
Kevin Schans <kevinvdsd@hotmail.com>
2024-10-03 01:55:53 +0200

    Create ErrorProvider context

---
+++ src/shared/context/ErrorProvider.tsx
@@ -0,0 +1,22 @@
+import { createContext, useContext, ReactElement, useCallback } from 'react'
+
+import { errorService } from '@errors/ErrorService'
+import { ErrorContextProps, ErrorProviderProps } from '@sharedTypes/ErrorTypes'
+
+const ErrorContext = createContext<ErrorContextProps | undefined>(undefined)
+
+export const ErrorProvider = ({ children }: ErrorProviderProps): ReactElement => {
+  const handleError = useCallback((error: Error) => {
+    errorService.handleError(error)
+  }, [])
+
+  return <ErrorContext.Provider value={{ handleError }}>{children}</ErrorContext.Provider>
+}
+
+export const useError = (): ErrorContextProps => {
+  const context = useContext(ErrorContext)
+  if (!context) {
+    throw new Error('useError must be used within an ErrorProvider')
+  }
+  return context
+}
