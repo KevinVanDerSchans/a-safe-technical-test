d5e32a5330c09a53d5efc68bcbfaf8193fdfbe68
Kevin Schans <kevinvdsd@hotmail.com>
2024-10-02 12:46:02 +0200

    Add new env variable for Customers data

--- src/infrastructure/config/env.ts
+++ src/infrastructure/config/env.ts
@@ -1,3 +1,3 @@
 export const NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL as string
-
+export const NEXT_PUBLIC_CUSTOMERS_API_URL = process.env.NEXT_PUBLIC_CUSTOMERS_API_URL
 export const NEXT_PUBLIC_AVATARS_URL = process.env.NEXT_PUBLIC_AVATARS_URL as string
