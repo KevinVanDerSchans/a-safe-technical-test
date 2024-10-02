572ede31f82afccc5ede68642af61afe6f277a08
Kevin Schans <kevinvdsd@hotmail.com>
2024-10-02 17:41:58 +0200

    Adjust seed for data consistency

--- src/infrastructure/repositories/customers/CustomersRepository.ts
+++ src/infrastructure/repositories/customers/CustomersRepository.ts
@@ -7,8 +7,8 @@ export class CustomersRepository implements Repository<Customer> {
     this.url = `${this.url}`
   }
 
-  async getAll(page = 1, resultsPerPage = 10): Promise<Customer[]> {
-    const response = await fetch(`${this.url}?page=${page}&results=${resultsPerPage}`)
+  async getAll(page = 1, resultsPerPage = 9, seed = 'default-seed'): Promise<Customer[]> {
+    const response = await fetch(`${this.url}?page=${page}&results=${resultsPerPage}&seed=${seed}`)
 
     if (!response.ok) {
       const message = `Error: ${response.status}. ${response.statusText}`
