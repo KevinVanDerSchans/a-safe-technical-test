6eb5053e3b4effdaa2a5c64882feabd0618c8351
Kevin Schans <kevinvdsd@hotmail.com>
2024-10-02 12:49:44 +0200

    Add customers reducer to reducers.ts file

--- src/infrastructure/redux/store/reducers.ts
+++ src/infrastructure/redux/store/reducers.ts
@@ -1,7 +1,9 @@
 import postsReducer from '@redux/slices/posts/postsSlice'
 import usersReducer from '@redux/slices/users/usersSlice'
+import customersReducer from '@redux/slices/customers/customersSlice'
 
 export const rootReducer = {
   posts: postsReducer,
   users: usersReducer,
+  customers: customersReducer,
 }
