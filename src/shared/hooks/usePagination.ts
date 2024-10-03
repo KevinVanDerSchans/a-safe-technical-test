7b134d12f81702182460ef772067badce7bb3cbd
Kevin Schans <kevinvdsd@hotmail.com>
2024-10-03 14:10:42 +0200

    Add error handling to pagination files

--- src/shared/hooks/usePagination.ts
+++ src/shared/hooks/usePagination.ts
@@ -1,16 +1,28 @@
 import { useState, useCallback } from 'react'
 import { CUSTOMERS_PAGINATION_RESULTS_PER_PAGE } from '@config/env'
+import { errorService } from '@app/services/errors/ErrorService'
+import PaginationErrors from '@customErrors/PaginationErrors'
 
 export function usePagination(initialPage = 1) {
   const [page, setPage] = useState(initialPage)
   const resultsPerPage = CUSTOMERS_PAGINATION_RESULTS_PER_PAGE
 
   const handleNextPage = useCallback(() => {
-    setPage(prevPage => prevPage + 1)
+    try {
+      setPage(prevPage => prevPage + 1)
+    } catch (error) {
+      errorService.handleError(new PaginationErrors.PaginationError())
+      throw error
+    }
   }, [])
 
   const handlePrevPage = useCallback(() => {
-    setPage(prevPage => (prevPage > 1 ? prevPage - 1 : 1))
+    try {
+      setPage(prevPage => (prevPage > 1 ? prevPage - 1 : 1))
+    } catch (error) {
+      errorService.handleError(new PaginationErrors.PaginationError())
+      throw error
+    }
   }, [])
 
   return {
