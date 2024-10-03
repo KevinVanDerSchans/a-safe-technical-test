cdc187178b525dfbfbcb3a3db5fb406dbde07716
Kevin Schans <kevinvdsd@hotmail.com>
2024-10-03 14:10:08 +0200

    Add error handling to LoginForm files

--- src/presentation/features/auth/hooks/useLoginForm.ts
+++ src/presentation/features/auth/hooks/useLoginForm.ts
@@ -1,7 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState } from 'react'
 import { signIn } from 'next-auth/react'
 import { useRouter } from 'next/router'
 import { showLoginSuccessAlert, showLoginErrorAlert } from '@utils/sweetalert2/sweetAlertConfig'
+import { errorService } from '@app/services/errors/ErrorService'
+import AuthErrors from '@customErrors/AuthErrors'
 
 const useLoginForm = () => {
   const [email, setEmail] = useState('')
@@ -11,23 +14,28 @@ const useLoginForm = () => {
 
   const handleLogin = async (credentials: { email?: string; password?: string }) => {
     setIsSubmitting(true)
+    try {
+      const result = await signIn('credentials', {
+        redirect: false,
+        email: credentials.email,
+        password: credentials.password,
+      })
 
-    const result = await signIn('credentials', {
-      redirect: false,
-      email: credentials.email,
-      password: credentials.password,
-    })
-
-    if (result?.ok) {
-      showLoginSuccessAlert()
-      router.push('/')
-    } else {
+      if (result?.ok) {
+        showLoginSuccessAlert()
+        router.push('/')
+      } else {
+        errorService.handleError(new AuthErrors.CredentialsError())
+        showLoginErrorAlert()
+        setEmail('')
+        setPassword('')
+      }
+    } catch (error) {
+      errorService.handleError(new AuthErrors.CredentialsError())
       showLoginErrorAlert()
-      setEmail('')
-      setPassword('')
+    } finally {
+      setIsSubmitting(false)
     }
-
-    setIsSubmitting(false)
   }
 
   const handleSubmit = async (e: React.FormEvent) => {
